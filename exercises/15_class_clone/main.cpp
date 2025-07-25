31b0f0cf323e08d82cc9bdddb2575703b4aa115a
zhangminyf <zhangminyf@wekoi.cn>
2025-07-25 14:09:20 +0800

    update

--- exercises/15_class_clone/main.cpp
+++ exercises/15_class_clone/main.cpp
@@ -10,17 +10,23 @@ class DynFibonacci {
 
 public:
     // TODO: 实现动态设置容量的构造器
-    DynFibonacci(int capacity): cache(new ?), cached(?) {}
+    DynFibonacci(int capacity): cache(new size_t[capacity]{0,1}), cached(2) {}
 
     // TODO: 实现复制构造器
-    DynFibonacci(DynFibonacci const &) = delete;
+    DynFibonacci(DynFibonacci const &other):cache(new size_t[other.cached]),cached(other.cached) {
+        for (int i = 0; i < cached; ++i) {
+            cache[i] = other.cache[i];
+        }
+    }
 
     // TODO: 实现析构器，释放缓存空间
-    ~DynFibonacci();
+    ~DynFibonacci() {
+        delete[] cache;
+    }
 
     // TODO: 实现正确的缓存优化斐波那契计算
     size_t get(int i) {
-        for (; false; ++cached) {
+        for (; cached <= i; cached++) {
             cache[cached] = cache[cached - 1] + cache[cached - 2];
         }
         return cache[i];
@@ -30,7 +36,7 @@ public:
     // NOTICE: 名字相同参数也相同，但 const 修饰不同的方法是一对重载方法，可以同时存在
     //         本质上，方法是隐藏了 this 参数的函数
     //         const 修饰作用在 this 上，因此它们实际上参数不同
-    size_t get(int i) const {
+    const size_t get(int i) const {
         if (i <= cached) {
             return cache[i];
         }
